72d3452e21d69f03f32634d1a6b6cd123ca1b6d3
Philippe Remy <premy.enseirb@gmail.com>
2021-01-25 09:14:49 +0900

    pub/sub

--- examples/perf/sub.py
+++ examples/perf/sub.py
@@ -8,5 +8,5 @@ m = MessageQueue()
 m.connect()
 m.subscribe('random')
 
-for i in tqdm(range(int(1e7)), desc='Benchmarking', file=sys.stdout, unit_scale=True, unit=' msg'):
+for i in tqdm(range(int(1e6)), desc='Benchmarking', file=sys.stdout, unit_scale=True, unit=' msg'):
     m.get_message(timeout=None)
