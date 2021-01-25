f411b99fabb4559ba30e3ba9d492a0f351477962
Philippe Remy <premy.enseirb@gmail.com>
2021-01-25 09:14:05 +0900

    pub/sub

--- examples/perf/pub.py
+++ examples/perf/pub.py
@@ -4,7 +4,7 @@ from pubsub import MessageQueue
 def main():
     m = MessageQueue()
     m.connect()
-    for i in range(int(1e9)):
+    for i in range(int(1e7)):
         m.send_message('random', f'My message {i}.')
 
 
